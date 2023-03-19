d362608b49771129c498c0293d84c20167c3c377
Vinicius Furukawa Carvalho <vinicius.furukawa.carvalho@gmail.com>
2023-03-19 20:53:27 -0300

    Organize project.  - Remove multiple storyboards.  - Remove code unused.  - Fix build.

---
+++ ComprasUSA/ViewControllers/NewProductViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewProductViewController.swift
+//  ComprasUSA
+//
+//  Created by Vinícius Furukawa Carvalho on 18/03/23.
+//
+
+import UIKit
+
+class NewProductViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ComprasUSA/ViewControllers/ShoppingSummaryViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ShoppingSummaryViewController.swift
+//  ComprasUSA
+//
+//  Created by Vinícius Furukawa Carvalho on 18/03/23.
+//
+
+import UIKit
+
+class ShoppingSummaryViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
