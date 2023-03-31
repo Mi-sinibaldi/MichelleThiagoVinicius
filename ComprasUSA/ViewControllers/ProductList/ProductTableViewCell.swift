3c827fce71d7cc0de95836130fc12a951ccadad2
Vinicius Furukawa Carvalho <vinicius.furukawa.carvalho@gmail.com>
2023-03-30 22:35:05 -0300

    summary with computation of dollar and brl values.

---
+++ ComprasUSA/ViewControllers/ProductList/ProductTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductTableViewCell.swift
+//  ComprasUSA
+//
+//  Created by Vinícius Furukawa Carvalho on 18/03/23.
+//
+
+import UIKit
+
+final class ProductTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var state: UILabel!
+    @IBOutlet weak var paymentType: UILabel!
+}
