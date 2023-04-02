3898354f39d62d1da9d2f42123e9ce7d7b8f4e2e
tmalcher <malcher.tm@gmail.com>
2023-04-02 18:45:53 -0400

    create subfolder to work process added state and %

--- ComprasUSA/ViewControllers/SettingsViewController.swift
+++ ComprasUSA/ViewControllers/SettingsViewController.swift
@@ -2,24 +2,22 @@
 //  SettingsViewController.swift
 //  ComprasUSA
 //
-//  Created by Vinícius Furukawa Carvalho on 18/03/23.
+//  Created by Thiago on 02/04/23.
 //
-
 
 import UIKit
 import CoreData
 
 class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var priceTextField: UITextField!
+    @IBOutlet weak var iofTextField: UITextField!
+    @IBOutlet weak var stateTableView: UITableView!
     
-    @IBOutlet weak var quotationTextField: UITextField!
-    @IBOutlet weak var financialTaxTextField: UITextField!
-    
-    @IBOutlet weak var statesTableView: UITableView!
-    
-    var states: [NSManagedObject] = [] {
+    var states: [StateEntity] = [] {
         didSet {
             DispatchQueue.main.async {
-                self.statesTableView.reloadData()
+                self.stateTableView.reloadData()
             }
         }
     }
@@ -30,8 +28,9 @@ class SettingsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.statesTableView.delegate = self
-        self.statesTableView.dataSource = self
+
+        self.stateTableView.delegate = self
+        self.stateTableView.dataSource = self
         
         NotificationCenter.default.addObserver(self, selector: #selector(self.defaultsChanged),
                                                name: UserDefaults.didChangeNotification, object: nil)
@@ -40,7 +39,7 @@ class SettingsViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        self.fetchStates()
+        self.retrieveStates()
         
         UserDefaults.standard.synchronize()
         self.updateScreenFields()
@@ -52,24 +51,19 @@ class SettingsViewController: UIViewController {
     
     private func updateScreenFields() {
         let defaults = UserDefaults.standard
-        self.quotationTextField.text = defaults.string(forKey: "quotation")
-        self.financialTaxTextField.text = defaults.string(forKey: "financial_tax")
+        self.priceTextField.text = defaults.string(forKey: "price")
+        self.iofTextField.text = defaults.string(forKey: "iof")
         
-        self.statesTableView.reloadData()
+        self.stateTableView.reloadData()
     }
     
-    private func fetchStates() {
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
-        
-        let managedContext = appDelegate.persistentContainer.viewContext
-        
-        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "StateEntity")
-
+    private func retrieveStates() {
+        let fetchRequest: NSFetchRequest<StateEntity> = StateEntity.fetchRequest()
         
         do {
-            self.states = try managedContext.fetch(fetchRequest)
+            self.states = try context.fetch(fetchRequest)
         } catch let error as NSError {
-            print("Could not fetch. \(error), \(error.userInfo)")
+            print("Failed to retrieve states. \(error), \(error.userInfo)")
         }
     }
     
@@ -101,44 +95,99 @@ class SettingsViewController: UIViewController {
         self.present(alertController, animated: true, completion: nil)
     }
     
-    private func saveNewState(name: String, tax: Double) {
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
-        let context = appDelegate.persistentContainer.viewContext
-
-        guard let entity = NSEntityDescription.entity(forEntityName: "StateEntity", in: context) else { return }
-
-        let state = NSManagedObject(entity: entity, insertInto: context)
+    private func editState(indexPath: Int, name: String, tax: Double) {
+        let state = self.states[indexPath]
+        state.tax = tax
+        state.name = name
         
-        state.setValue(name, forKeyPath: "name")
-        state.setValue(tax, forKeyPath: "tax")
-        
-        do {
-            try context.save()
-            self.states.append(state)
-        } catch let error as NSError {
-            print("Could not save. \(error), \(error.userInfo)")
+        if StateValidator.isValid(state, on: self) {
+            do {
+                try context.save()
+                retrieveStates()
+            } catch let error as NSError {
+                print("Error on save product. \(error), \(error.userInfo)")
+            }
+        } else {
+            context.rollback()
         }
     }
     
+    private func saveNewState(name: String, tax: Double) {
+        let state = StateEntity(context: self.context)
+        state.tax = tax
+        state.name = name
+        
+        if StateValidator.isValid(state, on: self) {
+            do {
+                try context.save()
+                retrieveStates()
+                navigationController?.popViewController(animated: true)
+            } catch let error as NSError {
+                print("Error on save product. \(error), \(error.userInfo)")
+            }
+        } else {
+            context.rollback()
+        }
+    }
 }
 
 extension SettingsViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        states.count
+       return states.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "State", for: indexPath) as? StateTableViewCell else {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "StateId", for: indexPath) as? SettingsStateTableView else {
             return UITableViewCell()
         }
         
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(rowTapped(_:)))
+        cell.addGestureRecognizer(tapGesture)
+                
         let state = states[indexPath.row]
         
-        cell.stateTextField?.text = state.value(forKeyPath: "name") as? String
-        cell.taxTextField?.text = String(state.value(forKeyPath: "tax") as? Double ?? 0.0)
+        cell.setName?.text = state.name
+        cell.setTaxa?.text = String(state.tax)
+        cell.tag = indexPath.row
         
         return cell
     }
+        
+    @objc func rowTapped(_ sender: UITapGestureRecognizer) {
+        guard let row = sender.view as? SettingsStateTableView else {
+            return
+        }
+        
+        let alertController = UIAlertController(title: "Editar Estado", message: "", preferredStyle: .alert)
+        
+        alertController.addTextField { (textField : UITextField!) -> Void in
+            textField.placeholder = "Nome do estado"
+            textField.text = row.setName.text
+        }
+        
+        alertController.addTextField { (textField : UITextField!) -> Void in
+            textField.placeholder = "Imposto"
+            textField.text = row.setTaxa.text
+        }
+        
+        let saveAction = UIAlertAction(title: "Salvar", style: .default, handler: { alert -> Void in
+            guard let fields = alertController.textFields, fields.count > 1 else { return }
+            let indexPath = IndexPath(row: row.tag, section: 0)
+            
+            let state = fields[0].text ?? ""
+            let tax = Double(fields[1].text ?? "0") ?? 0.0
+            
+            self.editState(indexPath: indexPath.row, name: state, tax: tax)
+        })
+        
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        
+        alertController.addAction(saveAction)
+        alertController.addAction(cancelAction)
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
@@ -148,9 +197,7 @@ extension SettingsViewController: UITableViewDataSource, UITableViewDelegate {
             let state = self.states[indexPath.row]
             context.delete(state)
             
-
             let fetchRequest: NSFetchRequest<ProductEntity> = ProductEntity.fetchRequest()
-
             let predicate = NSPredicate(format: "state == %@", state)
             fetchRequest.predicate = predicate
             
@@ -172,5 +219,4 @@ extension SettingsViewController: UITableViewDataSource, UITableViewDelegate {
             }
         }
     }
-    
 }
