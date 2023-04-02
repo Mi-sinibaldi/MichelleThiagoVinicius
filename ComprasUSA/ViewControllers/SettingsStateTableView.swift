d3dcaaa7f8771099d23cb8696149749d3eac81e9
tmalcher <malcher.tm@gmail.com>
2023-04-02 18:46:20 -0400

    create file to set name and set taxa

---
+++ ComprasUSA/ViewControllers/SettingsStateTableView.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsStateTableView.swift
+//  ComprasUSA
+//
+//  Created by Thiago on 02/04/23.
+//
+
+import Foundation
+import UIKit
+
+final class SettingsStateTableView: UITableViewCell {
+    @IBOutlet weak var setName: UILabel!
+    @IBOutlet weak var setTaxa: UILabel!}
