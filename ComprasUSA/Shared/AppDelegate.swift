06f33971512f817dd16b4a6ad43baa033e6f394f
tmalcher <malcher.tm@gmail.com>
2023-04-02 19:09:39 -0400

    added to show dollar price and ion in settings

--- ComprasUSA/Shared/AppDelegate.swift
+++ ComprasUSA/Shared/AppDelegate.swift
@@ -15,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        var appDefaults = [String:String]()
+        appDefaults["price"] = "5.3"
+        appDefaults["iof"] = "6.38"
+        UserDefaults.standard.register(defaults: appDefaults)
        
         return true
     }
